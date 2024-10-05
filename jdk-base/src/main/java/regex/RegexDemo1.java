8781d8b7a168c4798643f513b2eebb40c478f397
kurtis <xk233qwq@163.com>
2024-10-05 17:30:19 +0800

    regex

--- jdk-base/src/main/java/regex/RegexDemo1.java
+++ jdk-base/src/main/java/regex/RegexDemo1.java
@@ -1,8 +1,18 @@
 package regex;
 
-/// # 正则表达式
-///
-/// 例如他
+import java.util.regex.Pattern;
+
 public class RegexDemo1
 {
+    public static void main(String... args)
+    {
+        var pattern = Pattern.compile("a");
+        var matcher = pattern.matcher("java");
+
+        System.out.println(matcher.matches()); // 匹配整个字符串
+
+        // 寻找是否有匹配的子串
+        while (matcher.find())
+            System.out.println(matcher.start() + " " + matcher.end());
+    }
 }
